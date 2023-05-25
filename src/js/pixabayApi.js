a316cff44939d50b139317b3c69317ceb1c135d1
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-25 23:36:38 +0100

    Update pixabayApi.js

--- src/js/pixabayApi.js
+++ src/js/pixabayApi.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '36660019-c6df6bc1309e46eab5218ec9d';
 
-
 export default class searchImages {
   constructor() {
     this.page = 1;
@@ -14,7 +13,6 @@ export default class searchImages {
     const { data } = await axios.get(
       `${URL}?key=${API_KEY}&q=${this.values}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
     );
-    // console.log(data);
     this.incrementPage();
     this.totalHits = data.totalHits;
     return data.hits;
